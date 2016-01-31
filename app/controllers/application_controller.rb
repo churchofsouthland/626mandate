167a2be787ac2dbf3159d43eb3d917f6032d6f37
Daniel <daniel.kiros@gmail.com>
2016-01-30 16:06:14 -0800

    add new flash messaeg types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # toastr display types are "info, success, warning, error"
+  # add flash types to match
+  TOASTR_TYPES = [:info, :success, :warning, :error]
+  add_flash_types(*TOASTR_TYPES)
 end
