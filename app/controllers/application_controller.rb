1935c5a472e2d1ecc2c9b8ed3f9476437ff4438c
Daniel <daniel.kiros@gmail.com>
2016-02-06 21:29:12 -0800

    fix time zone code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  around_filter :set_time_zone
+
   # toastr display types are "info, success, warning, error"
   # add flash types to match
   TOASTR_TYPES = [:info, :success, :warning, :error]
   add_flash_types(*TOASTR_TYPES)
+
+
+  # set user's time_zone per request. default to PT.
+  def set_time_zone
+    old_time_zone = Time.zone
+    if current_user && current_user.try(:time_zone).present?
+      Time.zone = current_user.time_zone
+    else
+      Time.zone = Figaro.env.default_time_zone
+    end
+    yield
+  ensure
+    Time.zone = old_time_zone
+  end
 end
--- app/domain/date_utils.rb
+++ app/domain/date_utils.rb
@@ -1,18 +1,22 @@
 class DateUtils
+  # mandate start/end dates are in PT
   def self.mandate_start_date
-    DateTime.parse(Figaro.env.mandate_start_date).utc
+    Time.parse(Figaro.env.mandate_start_date).in_time_zone("Pacific Time (US & Canada)").beginning_of_day
   end
 
+  # mandate start/end dates are in PT
   def self.mandate_finish_date
-    DateTime.parse(Figaro.env.mandate_finish_date).utc
+    Time.parse(Figaro.env.mandate_finish_date).in_time_zone("Pacific Time (US & Canada)").end_of_day
   end
 
+  # calendar start/last dates are in user local time
   def self.calendar_start_date
-    DateTime.parse(Figaro.env.calendar_start_date).utc
+    Time.zone.parse(Figaro.env.calendar_start_date).beginning_of_day
   end
 
+  # calendar start/last dates are in user local time
   def self.calendar_last_date
-    DateTime.parse(Figaro.env.calendar_last_date).utc
+    Time.zone.parse(Figaro.env.calendar_last_date).end_of_day
   end
 
   def self.date_time_eq?(dt1, dt2)
