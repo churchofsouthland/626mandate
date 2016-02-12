4e7fd790aa2a7faa9fb8d97c274e02bc9347d26b
Daniel <daniel.kiros@gmail.com>
2016-02-11 17:05:34 -0800

    fix?

--- app/domain/date_utils.rb
+++ app/domain/date_utils.rb
@@ -1,12 +1,12 @@
 class DateUtils
   # mandate start/end dates are in PT
   def self.mandate_start_date
-    Time.parse(Figaro.env.mandate_start_date).in_time_zone("Pacific Time (US & Canada)").beginning_of_day
+    Time.zone.parse(Figaro.env.mandate_start_date).beginning_of_day
   end
 
   # mandate start/end dates are in PT
   def self.mandate_finish_date
-    Time.parse(Figaro.env.mandate_finish_date).in_time_zone("Pacific Time (US & Canada)").end_of_day
+    Time.zone.parse(Figaro.env.mandate_finish_date).end_of_day
   end
 
   # calendar start/last dates are in user local time
